8b110e07a29d325407f5227ad081e21ed5eb56f1
andrewfeikema <43078373+andrewfeikema@users.noreply.github.com>
2019-11-15 12:11:01 -0500

    Separated GUI functionality from background

--- MSGUI.py
+++ MSGUI.py
@@ -13,11 +13,6 @@ class GUI:
 
         # creates object for helper functions
         self.sweep = Sweeper(window, width, height, bombs)
-
-        self.flagsrem = IntVar()
-        self.flagsrem.set(bombs)
-        
-        self.firstturn = True
         
         '''Creates label and options for player 1'''
         p1label = Label(window, text="Player 1", bg='#FF0000')
@@ -39,20 +34,15 @@ class GUI:
         p2quit = Button(window, text="Quit", command=self.window.destroy)
         p2quit.grid(row=3, column=width-2, columnspan=2, sticky=E)
 
-        '''Starts the Timer and sets the status label'''
-        self.sweep.timer.set(5)
-        self.sweep.status.set("Pass to Player 1:")
         statuslabel1 = Label(window, text="Timer:")
         statuslabel1.grid(row=0, column=int(width/2)-3, columnspan=3, rowspan=2)
         statuslabel3 = Label(window, textvariable=self.sweep.timer)
         statuslabel3.grid(row=2, column=int(width/2)-3, columnspan=3)
-        '''runs in loop until game terminated'''
-        self.changetimer()
         
         '''Label for number of flags yet to be placed'''
         flaglabel = Label(window, text='Flags\nleft:')
         flaglabel.grid(row=0, column=int(width/2), rowspan=2, columnspan=3)
-        flagnumlabel = Label(window, textvariable=self.flagsrem)
+        flagnumlabel = Label(window, textvariable=self.sweep.flagsrem)
         flagnumlabel.grid(row=2, column=int(width/2), rowspan=1, columnspan=3)
         
         '''Label for the game's turn status and the win message'''
@@ -62,185 +52,14 @@ class GUI:
         '''Creates a range of buttons for the minesweeper board'''
         for y in range(height):
             for x in range(width):
-                b = Button(window, width=2, height=1, command=self.click_event, bg='#BBBBBB')
-                b.bind('<Button-3>', self.flag_event)
+                b = Button(window, width=2, height=1, command=self.sweep.click_event, bg='#BBBBBB')
+                b.bind('<Button-3>', self.sweep.flag_event)
                 b.grid(row=5+y, column=x)
                 self.sweep.buttonlist.append(b)
-        self.click_event()
-    
-    def changetimer(self):
-        """Controls the time status of each turn in the cycle
-        The status that this program changes cyclically controls many of the game's methods"""
-        # Stops sweep.timer if the game has been won
-        self.sweep.timer.set(self.sweep.timer.get() - 1)
-        if self.sweep.done:
-            return None
-        self.window.after(1000, self.changetimer)
-        # Once the time for a status elapses, the status is changed
-        if self.sweep.timer.get() == 0:
-            if self.sweep.status.get() == "Pass to Player 1:":
-                self.sweep.player1passed = False
-                self.sweep.timer.set(16)
-                # sets the first turn to half a regular turn to enforce fairness
-                if self.firstturn:
-                    self.sweep.timer.set(8)
-                    self.firstturn = False
-                self.sweep.status.set("Player 1's turn")
-            elif self.sweep.status.get() == "Player 1's turn":
-                self.sweep.timer.set(8)
-                """as a turn progresses, points are won and lost by placing and removing flags. Positive point values 
-                result from correct choices, negative values from incorrect choices to prevent the determination of flag
-                placement using the score, the score is compounded and written at the end of each turn."""
-                self.sweep.p1score += self.sweep.tempp1score
-                self.sweep.tempp1score = 0
-                self.sweep.p1scorelbl.set("Score: "+str(self.sweep.p1score))
-                self.sweep.status.set("Pass to Player 2:")
-            elif self.sweep.status.get() == "Pass to Player 2:":
-                self.sweep.player2passed = False
-                self.sweep.timer.set(16)
-                self.sweep.status.set("Player 2's turn")
-            elif self.sweep.status.get() == "Player 2's turn":
-                self.sweep.timer.set(8)
-                self.sweep.p2score += self.sweep.tempp2score
-                self.sweep.tempp2score = 0
-                self.sweep.p2scorelbl.set("Score: "+str(self.sweep.p2score))
-                self.sweep.status.set("Pass to Player 1:")
+        self.sweep.click_event()
 
-    def click_event(self):
-        """sets the first click to a random open square to prevent the first player from losing unfairly in the first
-        click"""
-        if self.sweep.firstclickstatus:
-            position = self.sweep.firstclick(self.width, self.height)
-            self.sweep.firstclickstatus = False
-        else:
-            if self.sweep.status.get() == "Pass to Player 1:" or self.sweep.status.get() == "Pass to Player 2:":
-                return None
-            # finds the position of the cursor upon click
-            x = self.window.winfo_pointerx() - self.window.winfo_rootx()
-            y = self.window.winfo_pointery() - self.window.winfo_rooty()
-            # calculates cell based on cursor position
-            cellx = x//24
-            celly = (y-94)//26
-            position = cellx + celly*self.width
-        
-        # removes clicked cell from grid
-        self.sweep.buttonlist[position].grid_forget()
-        
-        statnum = self.sweep.data[position][0]
-        # color for revealed number indicating neighboring bombs
-        color = self.sweep.setcolor(statnum)
-        # creates new label for clicked cell
-        d = Label(self.window, width=2, bg='white', text=statnum, height=1, border=3, pady=2, font='arial 9 bold',
-                  fg=color)
-        d.grid(row=5 + position // self.width, column=position % self.width)
-        # triggers a loss for clicking on a button above a bomb
-        if statnum == "*":
-            self.win("Bomb")
-        self.sweep.clickedlist.append(position)
-        self.sweep.clearing_loop(position)
-    
-    def flag_event(self):
-        """finds the position of the cursor"""
-        x = self.window.winfo_pointerx() - self.window.winfo_rootx()
-        y = self.window.winfo_pointery() - self.window.winfo_rooty()
-        cellx = x//24
-        celly = (y-94)//26
-        position = cellx + celly*self.width
-        
-        # sets different flagging effects for different turn status
-        if self.sweep.status.get() == "Player 1's turn":
-            self.flagsrem.set(self.flagsrem.get()-1)
-            bgcol = '#FF0000'
-            if self.sweep.data[position][0] == "*":
-                # 1 point for correctly placed flag
-                self.sweep.tempp1score += 1
-            else:
-                # -5 points for incorrectly plaged flag
-                self.sweep.tempp1score -= 5
-        
-        elif self.sweep.status.get() == "Player 2's turn":
-            self.flagsrem.set(self.flagsrem.get()-1)
-            bgcol = '#3054C6'
-            if self.sweep.data[position][0] == "*":    
-                self.sweep.tempp2score += 1
-            else:
-                self.sweep.tempp2score -= 5
-        # stops program if clicking happens during a passing status
-        else:
-            return None
-        
-        # forgets the old button in the position
-        self.sweep.buttonlist[position].grid_forget()
-        b = Button(self.window, width=2, bg=bgcol)
-        # adds the new button in the position
-        b.bind('<Button-3>', self.unflag_event)
-        b.grid(row=5+celly, column=cellx)
-        # adds the new button to the list of flag buttons to be accessed for unflagging
-        self.sweep.flagbuttonlist.pop(position)
-        self.sweep.flagbuttonlist.insert(position, b)
-        # checks if there are no more flags. In that case, the game is won
-        if self.flagsrem.get() == 0:
-            self.win('Zero flags')
-            
-    def unflag_event(self):
-        """Removes the flag from a button if right-clicked"""
-        # Finds position of cell clicked
-        x = self.window.winfo_pointerx() - self.window.winfo_rootx()
-        y = self.window.winfo_pointery() - self.window.winfo_rooty()
-        cellx = x//24
-        celly = (y-130)//26
-        position = cellx + celly*self.width
-        
-        if self.sweep.status.get() == "Player 1's turn":
-            self.flagsrem.set(self.flagsrem.get()+1)
-            if self.sweep.data[position][0] == "*":
-                # -1 point for removing a correctly placed flag
-                self.sweep.tempp1score -= 1
-            else:
-                # 5 points for removing an incorrectly placed flag
-                self.sweep.tempp1score += 5
-        elif self.sweep.status.get() == "Player 2's turn":
-            self.flagsrem.set(self.flagsrem.get()+1)
-            if self.sweep.data[position][0] == "*":    
-                self.sweep.tempp2score -= 1
-            else:
-                self.sweep.tempp2score += 5
-        else:
-            return None
-        # forgets flag button and restores unflagged button to grid
-        self.sweep.flagbuttonlist[position].grid_forget()
-        self.sweep.buttonlist[position].grid(row=5+celly, column=cellx)
-    
-    def win(self, cause):
-        """ends game given a cause"""
-        # updates scores
-        self.sweep.p1score += self.sweep.tempp1score
-        self.sweep.p2score += self.sweep.tempp2score
-        self.sweep.p1scorelbl.set("Score: " + str(self.sweep.p1score))
-        self.sweep.p2scorelbl.set("Score: " + str(self.sweep.p2score))
-        # sets boolean value to stop sweep.timer
-        self.sweep.done = True
-        if cause == 'Bomb':
-            if self.sweep.status.get() == "Player 1's turn" or self.sweep.status.get() == "Pass to Player 2":
-                self.sweep.status.set("Player 2 wins by explosion")
-            if self.sweep.status.get() == "Player 2's turn" or self.sweep.status.get() == "Pass to Player 1":
-                self.sweep.status.set("Player 1 wins by explosion!")
-        elif cause == 'Zero flags' or cause == 'Double pass':
-            if self.sweep.p1score > self.sweep.p2score:
-                self.sweep.status.set("{}. Player 1 wins! Score: {} - {}"
-                                      .format(cause, self.sweep.p1score, self.sweep.p2score))
-            elif self.sweep.p2score > self.sweep.p1score:
-                self.sweep.status.set("{}. Player 2 wins! Score: {} - {}"
-                                      .format(cause, self.sweep.p1score, self.sweep.p2score))
-            else:
-                self.sweep.status.set("{}. Tie game at {} - {}".format(cause, self.sweep.p1score, self.sweep.p2score))
-        
-        for child in self.window.winfo_children():
-            '''Disables all buttons except for the quit buttons at index 3 and 7'''
-            if child.winfo_class() == 'Button' and child != self.window.winfo_children()[3] and child != \
-                    self.window.winfo_children()[7]:
-                child.configure(state='disable')
-                child.unbind('<Button-3>')
+        '''runs in loop until game terminated'''
+        self.sweep.changetimer()
 
 
 if __name__ == '__main__':
--- sweeper.py
+++ sweeper.py
@@ -1,6 +1,6 @@
 """This program contains all the helper functions for MSGUI.py"""
 import random
-from tkinter import Label, StringVar, IntVar
+from tkinter import Label, StringVar, IntVar, Button
 
 
 class Sweeper:
@@ -9,26 +9,29 @@ class Sweeper:
         self.width = width
         self.height = height
 
+        self.flagsrem = IntVar(value=bombnum)
+
+        # one of 4 turn statuses
         self.status = StringVar()
-        self.timer = IntVar()
+        self.status.set("Pass to Player 1:")
+
+        '''Starts the Timer and sets the status label'''
+        self.timer = IntVar(value=5)
 
         self.p1score = 0
-        self.tempp1score = 0
+        self.temp1score = 0
         self.p2score = 0
-        self.tempp2score = 0
+        self.temp2score = 0
 
-        self.p1scorelbl = StringVar()
-        self.p2scorelbl = StringVar()
-        self.p1scorelbl.set("Score: 0")
-        self.p2scorelbl.set("Score: 0")
+        self.p1scorelbl = StringVar(value="Score: 0")
+        self.p2scorelbl = StringVar(value="Score: 0")
 
         # Initializes lists of clicked buttons, un-clicked buttons, and flagged buttons
         self.clickedlist = []
         self.buttonlist = []
-        self.flagbuttonlist = []
-        for x in range(height * width):
-            self.flagbuttonlist.append('')
+        self.flagbuttonlist = [None]*(height*width)
 
+        self.firstturn = True
         self.firstclickstatus = True
         self.done = False
         self.player1passed = False
@@ -69,8 +72,29 @@ class Sweeper:
         '''Changes the zeros to blank spaces to be written on the main board's labels'''
         data = [w.replace('0\n', ' \n') for w in data]
         self.data = data
-
-    def findneighbors(self, x, width, squarenum):
+    
+    @staticmethod
+    def setcolor(statnum):
+        """Returns the color associated with the number of bombs surrounding a minesweeper cell"""
+        col = 'black'
+        if statnum == '1':
+            col = 'blue'
+        elif statnum == '2':
+            col = 'green'
+        elif statnum == '3':
+            col = 'red'
+        elif statnum == '4':
+            col = 'purple'
+        elif statnum == '5':
+            col = 'maroon'
+        elif statnum == '6':
+            col = 'turquoise'
+        elif statnum == '8':
+            col = 'gray'
+        return col
+    
+    @staticmethod
+    def findneighbors(x, width, squarenum):
         """Finds the position in status.txt associated with the neighbors of a given cell, eliminating neighbors if on
         an edge or corner"""
         neighbors = [x - 1 - width, x - width, x + 1 - width, x - 1, x + 1, x - 1 + width, x + width, x + 1 + width]
@@ -111,25 +135,6 @@ class Sweeper:
             pos = random.randint(0, width * height - 1)
         return pos
 
-    def setcolor(self, statnum):
-        """Returns the color associated with the number of bombs surrounding a minesweeper cell"""
-        col = 'black'
-        if statnum == '1':
-            col = 'blue'
-        elif statnum == '2':
-            col = 'green'
-        elif statnum == '3':
-            col = 'red'
-        elif statnum == '4':
-            col = 'purple'
-        elif statnum == '5':
-            col = 'maroon'
-        elif statnum == '6':
-            col = 'turquoise'
-        elif statnum == '8':
-            col = 'gray'
-        return col
-
     def clearing_loop(self, pos):
         """Recursive program that clears all the cells surrounding a cell with no surrounding bombs. It then tests those
          cells and repeats"""
@@ -137,7 +142,7 @@ class Sweeper:
         if statnum == ' ':
             for u in self.findneighbors(pos, self.width, self.width * self.height):
                 # if the button is not clicked or flagged:
-                if u not in self.clickedlist and self.flagbuttonlist[u] == '':
+                if u not in self.clickedlist and not self.flagbuttonlist[u]:
                     # changes the button to a label
                     self.clickedlist.append(u)
                     self.buttonlist[u].grid_forget()
@@ -181,13 +186,123 @@ class Sweeper:
         self.clickedlist.append(position)
         self.clearing_loop(position)
 
+    def flag_event(self, event):
+            """finds the position of the cursor"""
+            x = self.window.winfo_pointerx() - self.window.winfo_rootx()
+            y = self.window.winfo_pointery() - self.window.winfo_rooty()
+            cellx = x // 24
+            celly = (y - 94) // 26
+            position = cellx + celly * self.width
+
+            # sets different flagging effects for different turn status
+            if self.status.get() == "Player 1's turn":
+                self.flagsrem.set(self.flagsrem.get() - 1)
+                bgcol = '#FF0000'
+                if self.data[position][0] == "*":
+                    # 1 point for correctly placed flag
+                    self.temp1score += 1
+                else:
+                    # -5 points for incorrectly placed flag
+                    self.temp1score -= 5
+
+            elif self.status.get() == "Player 2's turn":
+                self.flagsrem.set(self.flagsrem.get() - 1)
+                bgcol = '#3054C6'
+                if self.data[position][0] == "*":
+                    self.temp2score += 1
+                else:
+                    self.temp2score -= 5
+            # stops program if clicking happens during a passing status
+            else:
+                return None
+
+            # forgets the old button in the position
+            self.buttonlist[position].grid_forget()
+            b = Button(self.window, width=2, bg=bgcol)
+            # adds the new button in the position
+            b.bind('<Button-3>', self.unflag_event)
+            b.grid(row=5 + celly, column=cellx)
+            # adds the new button to the list of flag buttons to be accessed for unflagging
+            self.flagbuttonlist.pop(position)
+            self.flagbuttonlist.insert(position, b)
+            # checks if there are no more flags. In that case, the game is won
+            if self.flagsrem.get() == 0:
+                self.win('Zero flags')
+        
+    def unflag_event(self, event):
+        """Removes the flag from a button if right-clicked"""
+        # Finds position of cell clicked
+        x = self.window.winfo_pointerx() - self.window.winfo_rootx()
+        y = self.window.winfo_pointery() - self.window.winfo_rooty()
+        cellx = x // 24
+        celly = (y - 94) // 26
+        position = cellx + celly * self.width
+
+        if self.status.get() == "Player 1's turn":
+            self.flagsrem.set(self.flagsrem.get() + 1)
+            if self.data[position][0] == "*":
+                # -1 point for removing a correctly placed flag
+                self.temp1score -= 1
+            else:
+                # 5 points for removing an incorrectly placed flag
+                self.temp1score += 5
+        elif self.status.get() == "Player 2's turn":
+            self.flagsrem.set(self.flagsrem.get() + 1)
+            if self.data[position][0] == "*":
+                self.temp2score -= 1
+            else:
+                self.temp2score += 5
+        else:
+            return None
+        # forgets flag button and restores unflagged button to grid
+        self.flagbuttonlist[position].grid_forget()
+        self.buttonlist[position].grid(row=5 + celly, column=cellx)
+    
+    def changetimer(self):
+        """Controls the time status of each turn in the cycle
+        The status that this program changes cyclically controls many of the game's methods"""
+        # Stops sweep.timer if the game has been won
+        self.timer.set(self.timer.get() - 1)
+        if self.done:
+            return None
+        self.window.after(1000, self.changetimer)
+        # Once the time for a status elapses, the status is changed
+        if self.timer.get() == 0:
+            if self.status.get() == "Pass to Player 1:":
+                self.player1passed = False
+                self.timer.set(16)
+                # sets the first turn to half a regular turn to enforce fairness
+                if self.firstturn:
+                    self.timer.set(8)
+                    self.firstturn = False
+                self.status.set("Player 1's turn")
+            elif self.status.get() == "Player 1's turn":
+                self.timer.set(8)
+                """as a turn progresses, points are won and lost by placing and removing flags. Positive point values 
+                result from correct choices, negative values from incorrect choices to prevent the determination of flag
+                placement using the score, the score is compounded and written at the end of each turn."""
+                self.p1score += self.temp1score
+                self.temp1score = 0
+                self.p1scorelbl.set("Score: "+str(self.p1score))
+                self.status.set("Pass to Player 2:")
+            elif self.status.get() == "Pass to Player 2:":
+                self.player2passed = False
+                self.timer.set(16)
+                self.status.set("Player 2's turn")
+            elif self.status.get() == "Player 2's turn":
+                self.timer.set(8)
+                self.p2score += self.temp2score
+                self.temp2score = 0
+                self.p2scorelbl.set("Score: "+str(self.p2score))
+                self.status.set("Pass to Player 1:")
+
     def player1pass(self):
         """Passes player 1's turn and updates score"""
         if self.status.get() == "Player 1's turn":
             self.player1passed = True
             self.timer.set(8)
-            self.p1score += self.tempp1score
-            self.tempp1score = 0
+            self.p1score += self.temp1score
+            self.temp1score = 0
             self.p1scorelbl.set("Score: " + str(self.p1score))
             self.status.set("Pass to Player 2:")
             # ends game in case of double pass
@@ -198,8 +313,8 @@ class Sweeper:
         if self.status.get() == "Player 2's turn":
             self.player2passed = True
             self.timer.set(8)
-            self.p2score += self.tempp2score
-            self.tempp2score = 0
+            self.p2score += self.temp2score
+            self.temp2score = 0
             self.p2scorelbl.set("Score: " + str(self.p2score))
             self.status.set("Pass to Player 1:")
             if self.player1passed:
@@ -208,8 +323,8 @@ class Sweeper:
     def win(self, cause):
         """ends game given a cause"""
         # updates scores
-        self.p1score += self.tempp1score
-        self.p2score += self.tempp2score
+        self.p1score += self.temp1score
+        self.p2score += self.temp2score
         self.p1scorelbl.set("Score: " + str(self.p1score))
         self.p2scorelbl.set("Score: " + str(self.p2score))
         # sets boolean value to stop timer
